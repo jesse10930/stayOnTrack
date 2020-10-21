b324330a1a68bc5ca7bb03952f29c3db1ddb51de
jesse10930 <ronda.jesse@gmail.com>
2020-10-20 21:25:05 -0400

    preparing to deploy

--- src/components/sessions/Sessions.js
+++ src/components/sessions/Sessions.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import SessionItem from './SessionItem';
+// import SessionItem from './SessionItem';
 import PropTypes from 'prop-types';
 
 const Sessions = ({ startBtn, sessList }) => {  
@@ -7,15 +7,32 @@ const Sessions = ({ startBtn, sessList }) => {
       // hide div when start button pressed
       !startBtn && (
         <div className='all-sessions-container'>
-          {/* map over session list prop keys, returning a session item component for each */}
+          {/* map over session list prop keys, returning a session item div for each */}
           {Object.keys(sessList).map((time) => (
-            // pass session end time and task list array to child
-            <SessionItem
-              id={Object.keys(sessList).indexOf(time) + 1}
-              key={Math.floor(Math.random() * 1000 + 1)}
-              time={time}
-              sessTasks={sessList[time]}
-            />
+            <div className='session-container' key={Math.floor(Math.random() * 1000 + 1)}>
+              <h3>
+                Session {Object.keys(sessList).indexOf(time) + 1} End - {time}
+              </h3>
+              <br></br>
+              <ul className='session-ul'>
+                {/* map over task list array, return a list item for each task */}
+                {sessList[time].map((task) => (
+                  <li
+                    className='session-task'
+                    key={Math.floor(Math.random() * 1000 + 1)}
+                  >
+                    {task}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            // SessionItem component kept disappearing on deployment, temp fix above
+            // <SessionItem
+            //   id={Object.keys(sessList).indexOf(time) + 1}
+            //   key={Math.floor(Math.random() * 1000 + 1)}
+            //   time={time}
+            //   sessTasks={sessList[time]}
+            // />
           ))}
         </div>
       )
