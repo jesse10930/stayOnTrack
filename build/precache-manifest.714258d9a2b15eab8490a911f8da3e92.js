1d58097c8eafccb10aaeddbb66be0b6fc7b30f07
jesse10930 <ronda.jesse@gmail.com>
2020-10-20 21:35:56 -0400

    preparing to deploy

---
+++ build/precache-manifest.714258d9a2b15eab8490a911f8da3e92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1653598cc761c46bb7d132425d8173ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce8de95d1e08bbc952fc",
+    "url": "/static/css/main.a64fc11e.chunk.css"
+  },
+  {
+    "revision": "25869edcaa2f4f892415",
+    "url": "/static/js/2.d079f907.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.d079f907.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce8de95d1e08bbc952fc",
+    "url": "/static/js/main.92ad6d64.chunk.js"
+  },
+  {
+    "revision": "8c89cece9e0edc0d1b7d",
+    "url": "/static/js/runtime-main.b89a7fbd.js"
+  }
+]);
